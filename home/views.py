ae9768f5fabb639d88d60831bb8ff09eb6fadc42
Jose <jotienoochieng@gmail.com>
2019-05-15 20:03:29 +0300

    updates

--- home/views.py
+++ home/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 
@@ -6,5 +7,6 @@ def home_view(request):
     return render(request, 'home.html', )
 
 
+@login_required()
 def index_view(request):
     return render(request, 'index.html')
